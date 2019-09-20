bf1951bbd77e0e8ef11a9f02474744ba09c488fd
petermirithu <pyra_m.k@yahoo.com>
2019-09-20 20:28:53 +0300

    create a days array in script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
+
+
+
+
 function validate() {
   var date= parseInt(document.forms["akan"]["date"] .value);
-  var month=parseInt(document.forms["akan"][month]);
-  var year=parseIn(document.forms["akan"]["date"] .value);
-  
+  var month=parseInt(document.forms["akan"][month] .value);
+  var year=parseInt(document.forms["akan"]["date"] .value);
+  var result=date, month, year ;
+  alert(result);
 }
