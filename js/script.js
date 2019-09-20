40006ab066a24de294cc907fe4c72deb03c9f2f6
petermirithu <pyra_m.k@yahoo.com>
2019-09-20 23:12:41 +0300

    remove id on all fields.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function makeArray(n) {
     this[i] = 0;
   }
 }
-var days=new makeArray(7);
+days=new makeArray(7);
   days[0]="Sunday"
   days[1]="Monday"
   days[2]="Tuesday"
@@ -13,7 +13,7 @@ var days=new makeArray(7);
   days[5]="Friday"
   days[6]="Saturday"
 
-var months=new makeArray(12)
+months=new makeArray(12)
  months[0]="January"
  months[1]="February"
  months[2]="March"
@@ -27,62 +27,59 @@ var months=new makeArray(12)
  months[10]="Novermber"
  months[11]="December"
 
- var maleNames
- maleNames[0]="Kwasi"
- maleNames[1]="Kwadwo"
- maleNames[2]="Kwabena"
- maleNames[3]="Kwaku"
- maleNames[4]="Yaw"
- maleNames[5]="Kofi"
- maleNames[6]="Kwame"
+//  var maleNames
+//  maleNames[0]="Kwasi"
+//  maleNames[1]="Kwadwo"
+//  maleNames[2]="Kwabena"
+//  maleNames[3]="Kwaku"
+//  maleNames[4]="Yaw"
+//  maleNames[5]="Kofi"
+//  maleNames[6]="Kwame"
 
- var femaleNames
- femaleNames[0]="Akosua"
- femaleNames[1]="Adwoa"
- femaleNames[2]="Abenaa"
- femaleNames[3]="Akau"
- femaleNames[4]="Yaa"
- femaleNames[5]="Afua"
- femaleNames[6]="Ama"
+//  var femaleNames
+//  femaleNames[0]="Akosua"
+//  femaleNames[1]="Adwoa"
+//  femaleNames[2]="Abenaa"
+//  femaleNames[3]="Akau"
+//  femaleNames[4]="Yaa"
+//  femaleNames[5]="Afua"
+//  femaleNames[6]="Ama"
 
 
-function validate() {
-  var date= parseInt(document.forms["akan"]["date"] .value, 10)
-  if ((date < 0) || (date >31)) {
-    alert("Day is out of Range!!!")
-  };
+function validate(form) {
+    var val1= parseInt(form.day.value, 10)
+    if ((val1 < 0) || (val1 >31)) {
+      alert("Day is out of Range!!!")
+    };
 
-  var month= parseInt(document.forms["akan"]["month"] .value, 10)
-  if ((month <0) || (month >12)) {
-    alert("Month is out of Range!!!")
-  };
-  
-  var month2=parseInt(document.forms["akan"]["month"] .value, 10);
+    var val2= parseInt(form.month.value, 10)
+    if ((val2 <0) || (val2 >12)) {
+      alert("Month is out of Range!!!")
+    };
+    
+    var val2x= parseInt(form.month.value, 10)
+    var val3= parseInt(form.year.value, 10)
+    if (val3 <1000) {
+      alert("The year is past whats expected.")
+    };
 
-  var year= parseInt(document.forms["akan"]["year"] .value, 10)
-  if (year <1000) {
-    alert("The year is past whats expected.")
-  };
+    if (val2 == 1) {
+      val2x = 13;
+      val3 = val3-1
+    };
+    if (val2==2) {
+      val2x= 14;
+      val3 = val3-1
+    };
+    var val4 = parseInt(((val2x+1)*3)/5, 10)
+    var val5 = parseInt(val3/4, 10) 
+    var val6 = parseInt(val3/100, 10)
+    var val7 = parseInt(val3/400, 10)
+    var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
+    var val9 = parseInt(val8/7, 10)
+    var val0 = val8-(val9*7)
 
-  if (month === 1) {
-    month2 = 13;
-    year = year-1
-  };
-  if (month===2) {
-    month2 = 14;
-    year = year-1
-  };
-  var var1 = parseInt(((month2+1)*3)/5, 10)
-  var var2 = parseInt(year/4, 10) 
-  var var3 = parseInt(year/100, 10)
-  var var4 = parseInt(year/400, 10)
-  var var5 = date+(month2*2)+var1+year+var2-var3+var4+2
-  var var6 = parseInt(var5/7, 10)
-  var var7 = val5-(var6*7)
-
-  var formResult1 = months[month]+" "+document.forms["akan"]["date"] .value +", "+document.forms["akan"]["year"] .value
-  var formResult2 = days[var7]
-  alert(formResult1)
-  alert(formResult2)
+    form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
+    form.result2.value = days[val0]
 }
 
