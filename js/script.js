8561342cc643db51583a58d9b911de30f0104082
petermirithu <pyra_m.k@yahoo.com>
2019-09-20 17:05:43 +0300

    add validate function to js script.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function validate() {
+  var date= parseInt(document.forms["akan"]["date"] .value);
+  var month=parseInt(document.forms["akan"][month]);
+  var year=parseIn(document.forms["akan"]["date"] .value);
+  
+}
