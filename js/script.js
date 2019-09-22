c3e6387a23c6f0069629944a223c6a9a42323789
petermirithu <pyra_m.k@yahoo.com>
2019-09-22 10:28:22 +0300

    change variable names.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
- function MakeArray(n) {
+ function pyraArray(n) {
   this.length = n;
   for (var i = 1; i <=n; i++) {
     this[i] = 0;
   }
 }
-days = new MakeArray(7);
+days = new pyraArray(7);
 days[0] = "Saturday"
 days[1] = "Sunday"
 days[2] = "Monday"
@@ -13,7 +13,7 @@ days[4] = "Wednesday"
 days[5] = "Thursday"
 days[6] = "Friday"
 
-months = new MakeArray(12);
+months = new pyraArray(12);
 months[1] = "January" 
 months[2] = "February" 
 months[3] = "March" 
@@ -32,54 +32,58 @@ var maleNames=["Kwame","Kwasi", "Kwadwo","Kwabena", "Kwaku", "Yaw", "Kofi"]
 var femaleNames=[ "Ama","Akosua", "Adwoa","Abenaa", "Akau","Yaa", "Afua"]
 
 function validate() {
-  var val1 = parseInt(form.day.value, 10)
-  if ((val1 <=0) || (val1 > 31)) {
+  var day1 = parseInt(form.day.value, 10)
+  if ((day1 <=0) || (day1 > 31)) {
      alert("The Date your entered Doesn't Exist!!!")
   }
 
-  var val2 = parseInt(form.month.value, 10)
-  if ((val2 <=0) || (val2 > 12)) {
+  var month1 = parseInt(form.month.value, 10)
+  if ((month1 <=0) || (month1 > 12)) {
      alert("The Month you entered Doesn't Exist!!!")
   }  
 
   var val2x = parseInt(form.month.value, 10)
   var today= new Date();
   var year3= today.getFullYear();
-  var val3 = parseInt(form.year.value, 10)
-
-  if ((val3 < 1000) || (val3 > year3)) {
+  var year1 = parseInt(form.year.value, 10)
+    if ((year1%4==0) && (month1==2) && (day1 >29)){
+      alert("Date entered is Invalid.")
+    } else if ((year1%4>=1) && (month1==2) && (day1>28)){
+      alert("Date entered is Invalid")
+    }
+  if ((year1 < 1000) || (year1 > year3)) {
      alert("The year is way past whats expected!!!")
   }
-  if (val2 == 1) {
+  if (month1 == 1) {
      val2x = 13;
-     val3 = val3-1
+     year1 = year1-1
   }
-  if (val2 == 2) {
+  if (month1 == 2) {
      val2x = 14;
-     val3 = val3-1
+     year1 = year1-1
   }
-  var val4 = parseInt(((val2x+1)*3)/5, 10)
-  var val5 = parseInt(val3/4, 10)
-  var val6 = parseInt(val3/100, 10)
-  var val7 = parseInt(val3/400, 10)
-  var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
-  var val9 = parseInt(val8/7, 10)
-  var val0 = val8-(val9*7)
+  var code1 = parseInt(((val2x+1)*3)/5, 10)
+  var code2 = parseInt(year1/4, 10)
+  var code3 = parseInt(year1/100, 10)
+  var code4 = parseInt(year1/400, 10)
+  var code5 = day1+(val2x*2)+code1+year1+code2-code3+code4+2
+  var code6 = parseInt(code5/7, 10)
+  var answer = code5-(code6*7)
 
   var akanName = ""
   var gender = form.gender.value;
   if (gender==="Male") {
-    akanName = maleNames[val0]
+    akanName = maleNames[answer]
   } else if (gender==="Female") {
-    akanName = femaleNames[val0]
+    akanName = femaleNames[answer]
   } else {
     alert("Invalid Input!!!")
   }
 
   form.akan.value =akanName
 
-  form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
-  form.result2.value = days[val0]
+  form.result1.value = months[month1]+" "+form.day.value +", "+form.year.value
+  form.result2.value = days[answer]
 
 }   
 
